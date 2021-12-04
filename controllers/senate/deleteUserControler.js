ecc717aa843375ac91d6291cc7c56aa3eca86e5a
Faruoq01 <faruk.aminu@mabevic.com>
2021-12-04 02:50:59 -0800

    update

---
+++ controllers/senate/deleteUserControler.js
@@ -0,0 +1,30 @@
+const RegisterModel = require('../../models/senate/registerModel');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/app');
+const { response } = require('express');
+
+const deleteAdmin = async(req, res) => {
+
+    try{
+        const {email} = req.body;
+        const token = req.header('authorization').split(' ')[1];
+        jwt.verify(token, config.appKey, function(error, done){
+            if(error) if(error) return res.status(500).json({Error: 'Authentication failed'});
+            if(done){
+                RegisterModel.model.remove({email: email}, function(error, users){
+                    if(error) return res.status(500).json({Error: 'Serve error'});
+                    
+                    res.status(200).json({
+                        code: 200,
+                        message: 'success',
+                        email: email
+                    })
+                });
+            }
+        });
+    }catch (e){
+        return res.status(500).json({ message: e.message })
+    }
+}
+
+module.exports = deleteAdmin;
--- routers/senate.js
+++ routers/senate.js
@@ -2,6 +2,8 @@ const express = require('express');
 const registerController = require('../controllers/senate/register');
 const loginController = require('../controllers/senate/login');
 const adminControllers = require('../controllers/senate/admin');
+const adminUpdateController = require('../controllers/senate/updateAdmin');
+const deleteUserControler = require('../controllers/senate/deleteUserControler');
 const cors = require('cors');
 
 module.exports = function(){
@@ -10,6 +12,8 @@ module.exports = function(){
     router.post('/register', cors(), registerController);
     router.post('/login', cors(), loginController);
     router.get('/admin', cors(), adminControllers);
+    router.post('/update', cors(), adminUpdateController);
+    router.post('/delete', cors(), deleteUserControler);
 
     return router;
 }
