98e192a69df25998c710b474c4aeb21e7b71853b
Faruoq01 <faruk.aminu@mabevic.com>
2021-12-19 06:36:36 +0100

    bug fix

--- controllers/senate/admin.js
+++ controllers/senate/admin.js
@@ -5,7 +5,12 @@ const config = require('../../config/app');
 const getAdmin = async(req, res) => {
     const token = req.header('authorization').split(' ')[1];
     jwt.verify(token, config.appKey, function(error, done){
-        if(error) if(error) return res.status(500).json({Error: 'Authentication failed'});
+        if(error) if(error) return res.status(401).json({
+            error:{ 
+                name:'TokenExpiredError', 
+                message:'Authentication failed'
+            }
+        });
         if(done){
             RegisterModel.model.find(function(error, users){
                 if(error) return res.status(500).json({Error: 'Serve error'});
